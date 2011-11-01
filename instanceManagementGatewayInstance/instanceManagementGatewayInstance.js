86502e59a6f68986dd56244645918c886588a3e1
kai <kai@dadb3d17-6438-4b08-a509-4ebcba33077c>
2011-11-01 16:23:52 +0000

    instanceManagementGatewayInstance implemented without jsp hacks

---
+++ instanceManagementGatewayInstance/instanceManagementGatewayInstance.js
@@ -0,0 +1,10 @@
+function Widget_instanceManagementGatewayInstance() {
+	
+	this.onReadyExtend = function() {
+		this.loadbalancerUrl = this.$widgetDiv.attr("loadbalancerUrl");
+		$(".loadbalancerUrl", this.$widgetDiv).html(this.loadbalancerUrl);
+	}
+	
+}
+
+Widget_instanceManagementGatewayInstance.prototype = globalProperties.widgetPrototype;
