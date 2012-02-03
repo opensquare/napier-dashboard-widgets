2b52e5fe7796ad414881fa80b373c188e42bf2b9
johnv <johnv@dadb3d17-6438-4b08-a509-4ebcba33077c>
2012-02-03 11:20:30 +0000

    Updated scheduler job upload widget

--- schedulerUploadJob/schedulerUploadJob.js
+++ schedulerUploadJob/schedulerUploadJob.js
@@ -17,6 +17,23 @@ function Widget_schedulerUploadJob() {
 	this.handleEvent = function(channel, event) {
 		this.dialogue.dialog("open");
 	}
+
+
+	this.uploadSchedulerJobFiles = function(){
+		widgetObject = this;
+		var actionUrl = widgetObject.schedulerUrl + "/uploadScript";
+		actionUrl = getRelativeUrl(actionUrl);
+		$(widgetObject).attr("action", actionUrl);
+		return ajaxSubmitForm(form, function(form, responseDoc){
+			var success = responseDoc.getElementsByTagName("success")[0].childNodes[0].nodeValue;
+			if(success=="true"){
+				$(form).html("<p class='ui-state-alert'>Uploaded successfully</p>");
+			}else{
+				var error = responseDoc.getElementsByTagName("error")[0].childNodes[0].nodeValue;
+				$(form).before("<p class='ui-state-alert'>File could not be uploaded: " + error + "</p>")
+			}
+		});
+	}
 	
 }
 
