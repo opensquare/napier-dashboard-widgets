3d2f6edf6a1624df0ca4e172ab8c4a46d16f05e1
johnv <johnv@dadb3d17-6438-4b08-a509-4ebcba33077c>
2012-02-15 09:43:49 +0000

    Updated Upload new job widget

--- newSchedNewJob/newSchedNewJob.js
+++ newSchedNewJob/newSchedNewJob.js
@@ -1,4 +1,3 @@
-
 function Widget_newSchedNewJob() {
 
 	this.dialogue = null;
@@ -10,34 +9,30 @@ function Widget_newSchedNewJob() {
 	}
 	
 	this.onReadyExtend = function() {
-	//	this.dialogue = $(".dialogue", this.$widgetDiv).dialog({
-	//		title: "New Scheduler Job",
-	//		autoOpen: false
-	//	});
-	//	
-	//	var widgetObject = this;
-	//	
-	//	$("form", this.dialogue).each(function() {
-	//		$(this).attr("action", getRelativeUrl(widgetObject.schedulerUrl + "/REST/jobs"));
-	//	}).submit(function() {
-	//		var form = this;
-	//		return ajaxSubmitForm(form, function(form, responseDoc) {
-	//			var success = responseDoc.getElementsByTagName("success")[0].childNodes[0].nodeValue;
-	//			if (success == "true") {
-	//				$(form).html("<p class='ui-state-alert'>Scheduled successfully</p>");
-	//				notifyChannelOfEvent("schedulerUpdated");
-	//			} else {
-	//				var error = responseDoc.getElementsByTagName("error")[0].childNodes[0].nodeValue;
-	//				$(form).before("<p class='ui-state-alert'>Job could not be scheduled: " + error + "</p>")
-	//			}
-	//		});
-	//	})
+
+                var widgetObject = this;
+                $("form").attr("action", getRelativeUrl(widgetObject.schedulerUrl + "/REST/jobs"));
+                $("form").submit(function() {
+                        var form = this;
+                                return ajaxSubmitForm(form, function(form, responseDoc) {
+                              var success = responseDoc.getElementsByTagName("success")[0].childNodes[0].nodeValue;
+                              if (success == "true") {
+                                      $(form).html("<p class='ui-state-alert'>Scheduled successfully</p>");
+                                      notifyChannelOfEvent("schedulerUpdated");
+                              } else {
+                                      var error = responseDoc.getElementsByTagName("error")[0].childNodes[0].nodeValue;
+                                      $(form).before("<p class='ui-state-alert'>Job could not be scheduled: " + error + "</p>")
+                              }
+                      });
+
+                });
+
 	}
 	
 	this.handleEvent = function(channel, event) {
 	//	this.dialogue.dialog("open");
 	}
-	
+
 }
 
 Widget_newSchedNewJob.prototype = globalProperties.widgetPrototype;
